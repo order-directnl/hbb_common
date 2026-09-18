0d403f54e42dda29254b3059280f27d5b1e0e6c6
Richard Borg <support@order-direct.nl>
2026-09-18 10:18:58 +0200

    Set Order-Direct RustDesk server defaults

--- src/config.rs
+++ src/config.rs
@@ -114,8 +114,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["kantoor.order-direct.nl"];
+pub const RS_PUB_KEY: &str = "h5rEy3FW6cIQ72B8EEPgK7IkcLJy6i6M7OEsISWDIBQ=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
